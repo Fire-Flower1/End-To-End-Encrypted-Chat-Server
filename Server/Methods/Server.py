127647130c2431bd70502e5d9fe0fcdc4e9eff47
Fire-Flower1 <86540158+Fire-Flower1@users.noreply.github.com>
2025-03-25 14:41:06 +0000

    Added clientthread

--- Server/Methods/Server.py
+++ Server/Methods/Server.py
@@ -19,10 +19,37 @@ class Server:
 
         self.server.listen(100)
 
-        #Inits a string for the latest message for usage when the clients grab the latest message.
-        self.NewestMessage = ""
+        #Inits a list of clients to broadcast messages to.
+        self.client_list = []
 
-    def clientthread(self):
-        pass
+    def clientthread(self, conn, addr):
+            
+        # sends a message to the client whose user object is conn 
+        conn.send("Welcome to this chatroom!") 
+ 
+        while True: 
+                try: 
+                    message = conn.recv(2048) 
+                    if message: 
+    
+                        """prints the message and address of the 
+                        user who just sent the message on the server 
+                        terminal"""
+                        print ("<" + addr[0] + "> " + message) 
+    
+                        # Calls broadcast function to send message to all 
+                        message_to_send = "<" + addr[0] + "> " + message 
+                        broadcast(message_to_send, conn) 
+    
+                    else: 
+                        """message may have no content if the connection 
+                        is broken, in this case we remove the connection"""
+                        remove(conn) 
+    
+                except: 
+                    continue
+
+    def broadcast():
+         pass
 
         
